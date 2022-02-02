dd1a586d0a9702456268868016067ee89fcda4d8
JPKitro <jpkitro@gmail.com>
2022-02-02 20:20:30 -0300

    remove cache

--- static/js/cache.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-	navigator.serviceWorker.register('/sw.js');
-}
-
-if (window.caches) {
-	caches.open('Images-v1').then((cache) => {
-		cache.addAll(['/index.html', '/static/']);
-	});
-}
